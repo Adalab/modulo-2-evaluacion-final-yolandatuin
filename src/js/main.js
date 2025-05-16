d96c2d23e66a41c91677c72316fffdcc43a64734
Yolanda Tuñón <yolandatuin@gmail.com>
2025-05-16 20:59:09 +0200

    actualización viernes

--- src/js/main.js
+++ src/js/main.js
@@ -9,29 +9,46 @@ const inputBuscar = document.querySelector(".js_input-buscar");
 let productos = [];
 
 //FUNCIONES 
-function pintarUnProducto(producto) {
+function pintarUnProducto(productosAPintar) {
   productosCentro.innerHTML += `
   <li>
         <div class="producto">
-          <img class="imagen" src="${producto.image}" alt="">
-          <p class="nombre-producto">${producto.title}</p>
-          <p class="precio-producto">${producto.price} €</p>
+          <img class="imagen" src="${productosAPintar.image}" alt="">
+          <p class="nombre-producto">${productosAPintar.title}</p>
+          <p class="precio-producto">${productosAPintar.price} €</p>
+          <button class="comprar">Comprar</button>
         </div>
   </li>`
 }
 
-function renderProductosCentrales() {
-for (let producto of productos) {
-  pintarUnProducto(producto);
+function renderProductosCentrales(productosAPintar) {
+productosCentro.innerHTML = '';
+for (let producto of productosAPintar) {
+  pintarUnProducto(producto); //Se pinta un solo producto. El parámetro tiene que ser un solo item del array. 
 }};
 
 //FUNCIONES MANEJADORAS
-function handleClick(event){
-  alert("funciona");
-}
+function handleClickBuscar(event){
+  event.preventDefault();
+  const valorBuscar = inputBuscar.value;
+  const resultadosBusqueda = productos.filter(producto => producto.title.toLowerCase().includes(valorBuscar.toLowerCase())); //Resultado de la búsqueda 
+  console.log(resultadosBusqueda);
+  renderProductosCentrales(resultadosBusqueda);
+};
+
+function handleClickComprar(event){
+  event.preventDefault();
+  event.target.innerText = "Eliminar";
+  event.target.classList.toggle('enCesta');
+
+};
+
 
 //EVENTOS
-botonBuscar.addEventListener("click", handleClick);
+
+
+botonBuscar.addEventListener("click", handleClickBuscar);
+productosCentro.addEventListener("click", handleClickComprar);
 
 //FETCH
 fetch('https://fakestoreapi.com/products')
@@ -40,5 +57,5 @@ fetch('https://fakestoreapi.com/products')
     productos = data;
     console.log("ya estaría");
   
-  renderProductosCentrales();
+  renderProductosCentrales(productos);
 });
