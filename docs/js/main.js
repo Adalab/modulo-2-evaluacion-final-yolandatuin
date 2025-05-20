da473baa12bc174b581a9ff3874627716e9b6f96
Yolanda Tuñón <yolandatuin@gmail.com>
2025-05-20 20:39:44 +0200

    Publish web

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,2 +1,23 @@
-console.log(">> Ready :)");
+const o=document.querySelector(".js_products-center"),n=document.querySelector(".js_products-basket"),u=document.querySelector(".js_button-search"),p=document.querySelector(".js_input-search");let c=[],e=[];function g(t){n.innerHTML+=`
+  <li id="${t.id}">
+        <div class="product">
+        <p id="${t.id}" class="delete-from-basket">X</p>
+          <img class="image" src="${t.image}" alt="">
+          <p class="name-product">${t.title}</p>
+          <p class="price-product">${t.price} €</p>
+          <div class="basket-counter">
+            <p class="basket-symbol js_basketProduct_more">+</p>
+            <p class="js_basketProduct_count">2</p>
+            <p class="basket-symbol js_basketProduct_more">-</p>
+          </div>
+        </div>
+  </li>`}function m(t){o.innerHTML+=`
+  <li id="${t.id}">
+        <div class="product">
+          <img class="image" src="${t.image}" alt="">
+          <p class="name-product">${t.title}</p>
+          <p class="price-product">${t.price} €</p>
+          <button class="buy js_buy" id="${t.id}">Comprar</button>
+        </div>
+  </li>`}function d(t){o.innerHTML="";for(let s of t)m(s)}function i(t){n.innerHTML="";for(let s of t)g(s)}function f(t){t.preventDefault();const s=p.value,a=c.filter(r=>r.title.toLowerCase().includes(s.toLowerCase()));console.log(a),d(a)}function k(t){if(t.preventDefault(),t.target.tagName==="BUTTON"){t.target.innerText="Eliminar",t.target.classList.toggle("inBasket"),t.target.classList.contains("inBasket")?t.target.innerText="Eliminar":t.target.innerText="Comprar";const s=t.target.id,a=c.find(l=>l.id===Number(s)),r=e.findIndex(l=>l.id===Number(s));r===-1?(e.push(a),localStorage.setItem("listBasket",JSON.stringify(e)),i(e)):(e.splice(r,1),localStorage.setItem("listBasket",JSON.stringify(e)),i(e))}}function b(t){if(t.preventDefault(),t.target.classList.contains("delete-from-basket")){const s=t.target.id,a=e.findIndex(r=>r.id===Number(s));e.splice(a,1),i(e)}}u.addEventListener("click",f);o.addEventListener("click",k);n.addEventListener("click",b);localStorage.getItem("products")===null&&fetch("https://fakestoreapi.com/products").then(t=>t.json()).then(t=>{c=t,d(c),i(e),localStorage.setItem("products",JSON.stringify(c))});localStorage.getItem("products")!=null&&(c=JSON.parse(localStorage.getItem("products")),d(c));localStorage.getItem("listBasket")!=null&&(e=JSON.parse(localStorage.getItem("listBasket")),i(e));
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,78 +1,145 @@
 'use strict';
 
 //CONSTANTES Y VARIABLES
-const productosCentro = document.querySelector(".js_productos-centro");
-const productosCesta = document.querySelector(".js_productos-cesta");
+const centerProducts = document.querySelector(".js_products-center");
+const basketProducts = document.querySelector(".js_products-basket");
 
-const botonBuscar = document.querySelector(".js_boton-buscar");
-const inputBuscar = document.querySelector(".js_input-buscar");
+const searchButton = document.querySelector(".js_button-search");
+const inputSearch = document.querySelector(".js_input-search");
 
-let productos = [];
-let listadoCesta = [];
+let products = [];
+let listBasket = [];
 
 //FUNCIONES 
-function pintarUnProducto(UnProductoAPintar, lugarDondePintar ) {
-  lugarDondePintar.innerHTML += `
-  <li id="${UnProductoAPintar.id}">
-        <div class="producto">
-          <img class="imagen" src="${UnProductoAPintar.image}" alt="">
-          <p class="nombre-producto">${UnProductoAPintar.title}</p>
-          <p class="precio-producto">${UnProductoAPintar.price} €</p>
-          <button class="comprar js_comprar" id="${UnProductoAPintar.id}">Comprar</button>
+function paintAProductBasket(OneProductToPAint) {
+  basketProducts.innerHTML += `
+  <li id="${OneProductToPAint.id}">
+        <div class="product">
+        <p id="${OneProductToPAint.id}" class="delete-from-basket">X</p>
+          <img class="image" src="${OneProductToPAint.image}" alt="">
+          <p class="name-product">${OneProductToPAint.title}</p>
+          <p class="price-product">${OneProductToPAint.price} €</p>
+          <div class="basket-counter">
+            <p class="basket-symbol js_basketProduct_more">+</p>
+            <p class="js_basketProduct_count">2</p>
+            <p class="basket-symbol js_basketProduct_more">-</p>
+          </div>
         </div>
   </li>`
 };
 
-function renderProductos(productosAPintar, lugarDondePintar) {
-lugarDondePintar.innerHTML = '';
-for (let producto of productosAPintar) {
-  pintarUnProducto(producto, lugarDondePintar); //Se pinta un solo producto. El parámetro tiene que ser un solo item del array. 
-}};
-
-
-//FUNCIONES MANEJADORAS
-function handleClickBuscar(event){
-  event.preventDefault();
-  const valorBuscar = inputBuscar.value;
-  const resultadosBusqueda = productos.filter(producto => producto.title.toLowerCase().includes(valorBuscar.toLowerCase())); //Resultado de la búsqueda 
-  console.log(resultadosBusqueda);
-  renderProductos(resultadosBusqueda, productosCentro);
+function paintAProductCenter(OneProductToPAint) {
+  centerProducts.innerHTML += `
+  <li id="${OneProductToPAint.id}">
+        <div class="product">
+          <img class="image" src="${OneProductToPAint.image}" alt="">
+          <p class="name-product">${OneProductToPAint.title}</p>
+          <p class="price-product">${OneProductToPAint.price} €</p>
+          <button class="buy js_buy" id="${OneProductToPAint.id}">Comprar</button>
+        </div>
+  </li>`
 };
 
-function handleClickComprar(event){  
+function renderProductsCenter(productsToPaint) {
+centerProducts.innerHTML = '';
+for (let product of productsToPaint) {
+  paintAProductCenter(product); //Se pinta un solo producto. El parámetro tiene que ser un solo item del array. 
+}};
+
+function renderProductsBasket(productsToPaint) {
+basketProducts.innerHTML = '';
+for (let product of productsToPaint) {
+  paintAProductBasket(product); //Se pinta un solo producto. El parámetro tiene que ser un solo item del array. 
+}};
+
+//FUNCIONES MANEJADORAS
+function handleClickSearch(event){
+  event.preventDefault();
+  const valueSearch = inputSearch.value;
+  const searchResults = products.filter(product => product.title.toLowerCase().includes(valueSearch.toLowerCase())); //Resultado de la búsqueda 
+  console.log(searchResults);
+  renderProductsCenter(searchResults);
+};
+
+function handleClickBuy(event){  
   event.preventDefault();
 
   if (event.target.tagName === "BUTTON" ) {
       event.target.innerText = "Eliminar";
-      event.target.classList.toggle('enCesta');
+      event.target.classList.toggle('inBasket');
+        
+    if (event.target.classList.contains('inBasket')) {
+      event.target.innerText = "Eliminar";
+    } else {
+      event.target.innerText = "Comprar";
+    }
 
-      const clickedId = event.currentTarget.id;
+      const clickedId = event.target.id;     
+      const clickedproductobjt = products.find((product) => product.id === Number(clickedId));  //Buscamos con el id el producto en al array productoos
       
-      const clickedProductObjt = productos.find((producto) => producto.id === event.target.id);
-      console.log(clickedProductObjt);
+      const basketIndex = listBasket.findIndex((product) => product.id === Number(clickedId)); //Miramos si este productoo está en la cesta
+
+      if (basketIndex === -1) {
+        listBasket.push(clickedproductobjt);
+        localStorage.setItem("listBasket", JSON.stringify(listBasket));
+      renderProductsBasket(listBasket);
+      } else { 
+        listBasket.splice(basketIndex, 1);
+        localStorage.setItem("listBasket", JSON.stringify(listBasket));
+      renderProductsBasket(listBasket);
+
+      }
+           
+};
 };
 
+function handleClickX(event) {
+event.preventDefault();
 
+if (event.target.classList.contains('delete-from-basket')) {
 
-
-
-
-};
-
+      const clickedId = event.target.id;     
+      const basketIndex = listBasket.findIndex((product) => product.id === Number(clickedId));
+      listBasket.splice(basketIndex,1);
+      renderProductsBasket(listBasket);
+}}
 
 //EVENTOS
 
 
-botonBuscar.addEventListener("click", handleClickBuscar);
-productosCentro.addEventListener("click", handleClickComprar);
+searchButton.addEventListener("click", handleClickSearch);
+centerProducts.addEventListener("click", handleClickBuy);
+basketProducts.addEventListener("click", handleClickX);
+
 
 //FETCH
+
+if (localStorage.getItem("products") === null ) {
 fetch('https://fakestoreapi.com/products')
   .then((response) => response.json())
   .then((data) => {
-    productos = data;
-    console.log("ya estaría");
+    products = data;
   
-  renderProductos(productos, productosCentro);
-  renderProductos(listadoCesta, productosCesta);
+  renderProductsCenter(products);
+  renderProductsBasket(listBasket);
+
+  //Almacenar en el local Storage
+  localStorage.setItem("products", JSON.stringify(products));
+/*   localStorage.setItem("listBasket", JSON.stringify(listBasket)); */
 });
+} 
+
+
+if (localStorage.getItem("products") != null){
+  products = JSON.parse(localStorage.getItem("products"));
+  renderProductsCenter(products);
+};
+
+if (localStorage.getItem("listBasket") != null){
+  listBasket = JSON.parse(localStorage.getItem("listBasket"));
+  renderProductsBasket(listBasket);
+};
+
+
+    
+    
